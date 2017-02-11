a182cf37b7ea841ca5cd46d72d2507e3d1b64f0a
adel <adelmostafavi76@yahoo.com>
2017-02-11 18:52:04 +0330

    urls fixed and menu partially added

--- web/views.py
+++ web/views.py
@@ -1,24 +1,25 @@
 from django.shortcuts import render
-from django.http import HttpResponse,HttpRequest
+from django.http import HttpResponse
 from .models import User
 from django.views.decorators.csrf import csrf_exempt
-from django.views.decorators.http import require_POST
-@csrf_exempt
+
 
 
 # Create your views here.
+
 def regist(request):
-    print("kire khar")
-    return render(request,"register.html")
+    print("alleh")
+    return render(request, "register.html")
 
-
+@csrf_exempt
 def submit(request):
-    print("kir")
     # if User.objects.filter(request.POST['user']).exists():
     #     existance = {'message' : 'username already exists'}
     #     return render(request,"register.html",existance)
     # else :
 
     newUser = User.objects.create(name=request.POST['name'],username=request.POST['user'],password=request.POST['passw'])
-    newUser.save()
-    return render(request,"register.html")
+    return render(request, "register.html")
+
+def menu(request):
+    return render(request, "menu.html")
