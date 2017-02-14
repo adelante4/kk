db40e930dcee7a2bf218dc9e297f5683a0863e4f
adel <adelmostafavi76@yahoo.com>
2017-02-14 20:11:58 +0330

    login page added with a temporary welcome page

--- web/urls.py
+++ web/urls.py
@@ -4,7 +4,8 @@ from . import views
 urlpatterns =[
     url(r'^register/', views.regist),
     url(r'^data/', views.submit),
-    url(r'^login/', views.login),
+    url(r'^usercheck/', views.login),
+    url(r'^login/', views.showLogin),
     url(r'', views.regist),
 
 ]
--- web/views.py
+++ web/views.py
@@ -13,13 +13,18 @@ def regist(request):
 @csrf_exempt
 def submit(request):
     if User.objects.filter(username=request.POST['user']).exists():
-        existance = {'message' : 'username already exists'}
-        return render(request,"register.html", existance)
+        return render(request, "register.html")
     else:
         newUser = User.objects.create(fname=request.POST['fname'],lname=request.POST['lname'],email = request.POST['email'],username=request.POST['user'],password=request.POST['passw'])
         return render(request, "register.html")
 
 @csrf_exempt
 def login(request):
+    if User.objects.filter(username=request.POST['user']).exists():
+        newUser = User.objects.filter(username=request.POST['user']).get()
+        if request.POST['passw'] == newUser.password:
+            print('true')
+    return render(request, "welcome.html")
 
+def showLogin(request):
     return render(request, "login.html")
