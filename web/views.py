60f7b03291fbb0a274fa076aca416edbc2c314ee
adel <adelmostafavi76@yahoo.com>
2017-02-16 14:32:50 +0330

    backend and frotend have a relation now!

--- web/views.py
+++ web/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,JsonResponse
 from .models import User
 from django.views.decorators.csrf import csrf_exempt
-
-
+import json
 
 # Create your views here.
 
+
 def regist(request):
     return render(request, "register.html")
+
 
 @csrf_exempt
 def submit(request):
@@ -18,14 +19,17 @@ def submit(request):
         newUser = User.objects.create(fname=request.POST['fname'],lname=request.POST['lname'],email = request.POST['email'],username=request.POST['user'],password=request.POST['passw'])
         return render(request, "register.html")
 
+
 @csrf_exempt
 def login(request):
     if User.objects.filter(username=request.POST['user']).exists():
         newUser = User.objects.filter(username=request.POST['user']).get()
         if request.POST['passw'] == newUser.password:
             return render(request, "welcome.html")
-    else:
-        return render(request,"login.html")
+
+    data = {'message': 'wrong username or password!'}
+    return render(request, "login.html", data)
+
 
 def showLogin(request):
     return render(request, "login.html")
